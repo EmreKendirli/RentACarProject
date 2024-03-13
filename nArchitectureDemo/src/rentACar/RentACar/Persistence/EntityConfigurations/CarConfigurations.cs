4a515a35daf32649ad4469ea361a567c3bfda531
EmreKendirli <emrekenderli@gmail.com>
2024-03-14 02:44:55 +0300

    first commit

---
+++ nArchitectureDemo/src/rentACar/RentACar/Persistence/EntityConfigurations/CarConfigurations.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.EntityConfigurations;
+
+public class CarConfigurations : IEntityTypeConfiguration<Car>
+{
+    public void Configure(EntityTypeBuilder<Car> builder)
+    {
+        builder.ToTable("Cars").HasKey(b => b.Id);
+
+        builder.Property(b => b.Id).HasColumnName("Id").IsRequired();
+        builder.Property(b => b.ModelId).HasColumnName("ModelId").IsRequired();
+        builder.Property(b => b.Kilometer).HasColumnName("Kilometer").IsRequired();
+        builder.Property(b => b.Plate).HasColumnName("Plate").IsRequired();
+        builder.Property(b => b.MinFindexScore).HasColumnName("MinFindexScore").IsRequired();
+        builder.Property(b => b.CarState).HasColumnName("State");
+        builder.Property(b => b.ModelYear).HasColumnName("ModelYear");
+
+
+        builder.Property(b => b.CreatedDate).HasColumnName("CreatedDate");
+        builder.Property(b => b.UpdatedDate).HasColumnName("UpdatedDate");
+        builder.Property(b => b.DeletedDate).HasColumnName("DeletedDate");
+
+
+        builder.HasOne(b => b.Model);//models alanı ile bire çok ilişki var
+
+        builder.HasQueryFilter(b => !b.DeletedDate.HasValue);
+    }
+}
---
+++ nArchitectureDemo/src/rentACar/RentACar/WebApi/Controllers/BrandsController.cs
@@ -0,0 +1,57 @@
+﻿using Application.Features.Brands.Commands.Create;
+using Application.Features.Brands.Commands.Delete;
+using Application.Features.Brands.Commands.Update;
+using Application.Features.Brands.Queries.GetById;
+using Application.Features.Brands.Queries.GetList;
+using Core.Application.Request;
+using Core.Application.Response;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : BaseController
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateBrandCommand createBrandCommand)
+        {
+            CreatedBrandResponse response = await Mediator.Send(createBrandCommand);
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListBrandQuery getListBrandQuery = new() { PageRequest = pageRequest };
+
+            GetListResponse<GetListBrandListItemDto> response = await Mediator.Send(getListBrandQuery);
+            return Ok(response);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] Guid id)
+        {
+            GetByIdBrandQuery getByIdBrandQuery = new() {  Id = id };
+            GetByIdBrandResponse response = await Mediator.Send(getByIdBrandQuery);
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdatedBrandCommand updatedBrandCommand)
+        {
+            UpdatedBrandResponse response = await Mediator.Send(updatedBrandCommand);
+
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] Guid id)
+        {
+            DeletedBrandResponse response = await Mediator.Send(new DeletedBrandCommand { Id = id });
+            return Ok(response);
+        }
+    }
+}
